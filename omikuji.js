47f580b15a4df0c6759494c5de8cf12ddd4d02c8
Makoto Tanabe <xekid78@outlook.com>
2018-03-17 11:45:43 +0900

    Add files via upload

---
+++ omikuji.js
@@ -0,0 +1,14 @@
+var omikuji = Math.floor(Math.random () * 6) + 1;
+if (omikuji === 1) {
+    console.log("大吉");
+} else if (omikuji === 2) {
+    console.log("中吉");
+} else if (omikuji === 3) {
+    console.log("小吉");
+} else if (omikuji === 4) {
+    console.log("吉");
+} else if (omikuji === 5) {
+    console.log("凶");
+} else {
+    console.log("大凶");
+}
